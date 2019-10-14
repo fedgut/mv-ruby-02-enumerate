e9fe13dc76ca51ca45aaacaaa2d671bb67fcae8f
fedgut <fedgut@gmail.com>
2019-10-14 16:08:34 -0500

    Add more test

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -6,6 +6,7 @@ require './lib/enum'
 
 ar = [1, 2, 3]
 ar2 = %w[ant bear cat]
+ar3 = [1, 'dog', 3]
 range = (1..3)
 empty = []
 
@@ -73,16 +74,40 @@ RSpec.describe Enumerable do
   end
 
   describe '#my_all' do
-    it 'the method returns whether pattern === element for every collection member' do
+    it 'Returns true for empty array and no block' do
+      expect(empty.my_all?).to be_truthy
+    end
+
+    it 'Returns true for empty array and any block' do
+      expect(empty.my_all?(&:even?)).to be_truthy
+    end
+
+    it 'Return true if all elem match the pattern' do
+      expect(ar2.my_all?(/a/)).to eql(true)
+    end
+
+    it 'Return false if any elem doesnt match the pattern' do
       expect(ar2.my_all?(/t/)).to eql(false)
     end
 
-    it 'Checks if all elements of the enum fulfill the block condition' do
+    it 'Return true if all elems in arr fulfill condition' do
       expect(ar2.my_all? { |word| word.length <= 4 }).to eql(true)
+    end
+
+    it 'Return false if any elems in arr  doesnt fulfill condition' do
+      expect(ar2.my_all? { |word| word.length < 4 }).to eql(false)
     end
   end
 
   describe 'my_any?' do
+    it 'Returns false if empty arr and no block' do
+      expect(empty.my_any?).to eql(false)
+    end
+
+    it 'Returns false if empty arr and any block' do
+      expect(empty.my_any?(&:even?)).to eql(false)
+    end
+
     it 'Checks if any element matches the pattern' do
       expect(ar2.my_any?(/b/)).to eql(true)
     end
@@ -93,6 +118,14 @@ RSpec.describe Enumerable do
   end
 
   describe 'my_none' do
+    it 'Returns true if empty arr and no block' do
+      expect(empty.my_none?).to eql(true)
+    end
+
+    it 'Returns true if empty arr and any block' do
+      expect(empty.my_none?(&:even?)).to eql(true)
+    end
+
     it 'Checks if no elem contains the pattern' do
       expect(ar2.my_none?(/b/)).to eql(false)
     end
@@ -100,9 +133,29 @@ RSpec.describe Enumerable do
     it 'Checks if no elements of the enum fulfill the block condition' do
       expect(ar2.my_none? { |word| word.length == 4 }).to eql(false)
     end
+
+    it 'Returns true if no elem in Array match' do
+      expect(ar2.my_none? { |word| word.length > 5 }).to eql(true)
+    end
+
+    it 'Returns false if any elem in range match' do
+      expect(range.my_none?(&:even?)).to eql(false)
+    end
+
+    it 'Returns false in any elem in the Array match Type' do
+      expect(ar3.my_none? { |elem| elem.is_a?(String) }).to eql(false)
+    end
+
+    it 'Returns false if any elem of array match equality' do
+      expect(ar3.my_none? { |elem| elem == 1 }).to eql(false)
+    end
   end
 
   describe 'my_count' do
+    it 'Returns zero on empty arr' do
+      expect(empty.my_count).to eql(0)
+    end
+
     it 'returns the number of elements in the enum' do
       expect(ar.my_count).to eql(3)
     end
@@ -121,14 +174,30 @@ RSpec.describe Enumerable do
       expect((1..4).my_map { |i| i * i }).to eql([1, 4, 9, 16])
     end
 
-    it 'Returns a new array with the results of running block once for every element in enum.' do
+    it 'Return an Enum if no block is given' do
       expect((1..4).my_map).to be_instance_of(Enumerator)
     end
   end
 
   describe 'my_inject' do
-    it 'accumulates and returns the result of passing each elem of the enumerator to a block or a symbol ' do
+    it 'Accumulate on passing a block' do
       expect((5..10).my_inject { |sum, i| sum + i }).to eql(45)
+    end
+
+    it 'Accumulate on passing a symbol' do
+      expect((5..10).my_inject(:+)).to eql(45)
+    end
+
+    it 'Accumulate on passing a symbol with arg' do
+      expect((5..10).my_inject(1, :+)).to eql(46)
+    end
+
+    it 'Accumulate on with arg and block' do
+      expect((5..10).my_inject(1) { |elem, i| elem + i }).to eql(46)
+    end
+
+    it 'Accumulate on with arg and block' do
+      expect(ar.my_inject(1) { |elem, i| elem + i }).to eql(7)
     end
   end
 end
